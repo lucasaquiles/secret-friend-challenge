785f81dd76189c643183d74338edb3b35c78d27e
Lucas Aquiles <lucasaquiles@gmail.com>
2019-12-21 21:07:16 -0300

    api definition

---
+++ secret-friend-api/src/main/kotlin/com/dev4fun/secretfriendapi/SecretFriendApiApplication.kt
@@ -0,0 +1,11 @@
+package com.dev4fun.secretfriendapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecretFriendApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecretFriendApiApplication>(*args)
+}
